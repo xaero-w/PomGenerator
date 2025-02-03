76fabb074a06e099909b9b11186edd7fe400ec90
xaero-w <d_shalimov@mail.ru>
2025-02-04 01:58:46 +0300

    bom копируется в двух вариантах - pom не копируется

--- lib/BomSaver.py
+++ lib/BomSaver.py
@@ -1,5 +1,3 @@
-#BomSaver.py
-
 import os
 import shutil
 import logging
@@ -8,25 +6,30 @@ import logging
 logger = logging.getLogger()
 
 class BomSaver:
-    def __init__(self, file_prefix):
+    def __init__(self, file_prefix, save_path):
         self.file_prefix = file_prefix
+        self.save_path = save_path  # Используем путь из формы
 
-    def move_bom_files(self):
-        """Перемещение файлов bom.json и bom.xml в нужную директорию."""
-        bom_json_path = os.path.join(os.getcwd(), f"{self.file_prefix}_bom.json")
-        bom_xml_path = os.path.join(os.getcwd(), f"{self.file_prefix}_bom.xml")
+    def copy_bom_files(self):
+        """Копирование файлов bom.json и bom.xml в указанный каталог."""
+        bom_json_path = os.path.join(self.save_path, f"{self.file_prefix}_bom.json")
+        bom_xml_path = os.path.join(self.save_path, f"{self.file_prefix}_bom.xml")
 
         target_bom_json = os.path.join(os.getcwd(), "target", "bom.json")
         target_bom_xml = os.path.join(os.getcwd(), "target", "bom.xml")
 
+        # Проверяем, существует ли директория, если нет — создаем
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
+
         if os.path.exists(target_bom_json):
-            shutil.move(target_bom_json, bom_json_path)
-            logger.info(f"Файл bom.json перемещен в {bom_json_path}")
+            shutil.copy(target_bom_json, bom_json_path)  # КОПИРУЕМ вместо move
+            logger.info(f"Файл bom.json скопирован в {bom_json_path}")
         else:
             logger.warning(f"Файл bom.json не найден в {target_bom_json}")
 
         if os.path.exists(target_bom_xml):
-            shutil.move(target_bom_xml, bom_xml_path)
-            logger.info(f"Файл bom.xml перемещен в {bom_xml_path}")
+            shutil.copy(target_bom_xml, bom_xml_path)  # КОПИРУЕМ вместо move
+            logger.info(f"Файл bom.xml скопирован в {bom_xml_path}")
         else:
             logger.warning(f"Файл bom.xml не найден в {target_bom_xml}")
